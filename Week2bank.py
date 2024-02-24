cc3c32dd8d6bf1357fd8bf8c0234b0934d9cbe24
Grainne10 <grainne_boyle@outlook.com>
2024-02-24 21:45:28 +0000

    Update weekly tasks week 2 and 3

---
+++ Week2bank.py
@@ -0,0 +1,15 @@
+# bank.py
+# This program adds two amounts together and gives the answer in a readable format  
+# author : Grainne Boyle
+
+
+number1 = float(input('please enter amount1: '))
+
+number2= float(input('please enter amount2: '))
+
+answer = (number1 + number2)
+print(F"The sum of these is €{answer}")
+
+
+
+
